05435f700f378daaa0293a642ed97c405a0a9a08
Nimeshariyarathne <106456842+Nimeshariyarathne@users.noreply.github.com>
2022-06-17 21:17:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = 94763449063
+global.premium = 94763449063
+global.ownernomer = 94763449063
+global.ownername = '🐦۝༺🅻🅾🆁🅳 𝐓𝐇𝐀𝐍𝐎𝐒༻۝'
+global.botname = '۝༺🅻🅾🆁🅳 𝐓𝐇𝐀𝐍𝐎𝐒༻۝'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = 'nimeshlakruwan'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
